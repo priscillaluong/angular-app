116aed57b25c86d64945545f05e8fedd415935ae
P Luong <p-luong@live.co.uk>
2022-10-19 22:10:24 +0100

    created task item components

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TASKS} from '../../mock-tasks';
+import {Task} from '../../Task';
 
 @Component({
   selector: 'app-tasks',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-
+  tasks: Task[] = TASKS;
+  
   constructor() { }
 
   ngOnInit(): void {
