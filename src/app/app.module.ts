461cb9c2a8ad23de529864ed385936d7a7711ce4
P Luong <p-luong@live.co.uk>
2022-10-19 23:37:37 +0100

    create add task functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,9 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HttpClientModule, HttpClient } from '@angular/common/http'
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { AddTaskComponent } from './components/add-task/add-task.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -16,12 +17,14 @@ import { HttpClientModule, HttpClient } from '@angular/common/http'
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
